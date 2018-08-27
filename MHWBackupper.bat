5dbcebf0767dea0142783ba5e9f22e1475dcd471
NuVanDibe <nuvandibe@gmail.com>
2018-08-26 23:15:29 -0500

    Changed from using "if exist" to using "dir" to avoid file path with spaces issues

--- MHWBackupper.bat
+++ MHWBackupper.bat
@@ -9,21 +9,22 @@ goto main
 )
 
 :main
-
+rem Get date & time and generate folder name:
 set tmpvar=%date:~10,4%-%date:~4,2%-%date:~7,2% %time:~0,2%.%time:~3,2%.%time:~6,2%
 if %tmpvar:~11,2% LSS 10 set tmpvar=%tmpvar:~0,11%0%tmpvar:~-7%
-
-if exist "%backuplocation%\%tmpvar%" rd /s /q "%backuplocation%\%tmpvar%"
-
+dir "%usrdata%%steamid%\582010" > nul || goto errornosource 
+dir "%backuplocation%\%tmpvar%" > nul && rd /s /q "%backuplocation%\%tmpvar%"
 xcopy /e /h /i "%usrdata%%steamid%\582010" "%backuplocation%\%tmpvar%\save"
-
 (
-echo if exist "%usrdata%%steamid%\582010" rd /s /q "%usrdata%%steamid%\582010"
+rem todo: the next line is run instead of being echoed?
+echo rd /s /q "%usrdata%%steamid%\582010"
 echo xcopy /e /h /i save "%usrdata%%steamid%\582010"
 )>"%backuplocation%\%tmpvar%\restore.bat"
 
-goto eof
 
+goto eof
+                                                                         
+          
 :setid
 echo This will generate a .bat file which will back up your Monster Hunter World save file to
 echo a directory of your choosing. It will generate a new backup every time you run the
@@ -44,27 +45,37 @@ echo.
 set numfolders=0
 for /f %%G in ('dir "%usrdata%*" /b') do (
 set sid=%%~G
-set numfolders=numfolders+1
+set /a numfolders=numfolders+1
 )
-echo You need to set the Steam ID. This is most likely %sid%, unless you've signed into multiple Steam accounts.
-set /p steamid=Type Steam ID: 
+if %numfolders%==2 (
+set steamid=%sid%
+echo SteamID3 auto-detected: %sid%
+) else (
+set choice=
+set /p choice=You need to set the SteamID3. Launch https://steamidfinder.com to locate SteamID3? Y/N:  
+)
+if /I "%choice%"=="Y" (
+start /max https://steamidfinder.com
+)
+if "%steamid%"=="" set /p steamid=Type SteamID3: 
+
 echo.
 echo You need to set the backup directory. Example: C:\Backups\Monster Hunter World
 echo Do not use a trailing backslash.
 echo.
 set /p backuplocation=Type Backup Directory: 
 
+:outfile
 echo.
+if not '%outputfile%'=='' echo File already exists. Try again.
 echo You need to set the output filename. (It must end in .bat or .cmd) Example: backupmhw.bat
 echo.
 set /p outputfile=Type Output Filename: 
-
-if exist %outputfile% (
-echo File already exists. Try again.
-set steamid=
-pause
+if exist %outputfile% goto outfile
+goto makebat
+:errornosource
+echo Could not find source directory: "%usrdata%%steamid%\582010" Please rerun setup (MHWBackupper.bat)
 goto eof
-)
 
 :makebat
 (
@@ -76,3 +87,8 @@ echo.
 TYPE %~nx0>>%outputfile%
 
 :eof
+set tmpvar=
+set steamid=
+set choice=
+set numfolders=
+set sid=
